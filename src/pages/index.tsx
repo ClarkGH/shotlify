d53cbfff26717ab9affb5f30e49172a5328e3e6d
clarkgh <clarkhinchcliff@gmail.com>
2023-02-03 16:23:23 -0500

    screenshots embed
    housekeeping
    
    housekeeping
    
    Add image file. Ready for upload.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import { type NextPage } from "next";
 import Image from "next/image";
 import Head from "next/head";
-import { useState } from "react";
+
 import FileInput from "../common/components/FileInput";
 
 // import Link from "next/link";
@@ -36,27 +37,54 @@ const Home: NextPage = () => {
     }
   }
 
+  const captureImageFromVideo = (index: number) => {
+    const video = document.querySelectorAll('video')[index];
+    const canvas = document.createElement('canvas');
+
+    if (!video || !canvas) return;
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    const ctx = canvas.getContext('2d');
+
+    ctx?.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const blob = new Blob([canvas.toDataURL('image/jpeg')], { type: 'image/jpeg' });
+    const blobFile = new File([blob], `screenshot_i_${index}.jpeg`);
+
+    setImages((prevImages) => [...prevImages, blobFile]);
+    setImageSources((prevImageSources) => [canvas.toDataURL('image/jpeg'), ...prevImageSources]);
+  }
+
   return (
     <>
       <Head>
         <title>Shotlify</title>
+
         <meta name="description" content="Take screenshots and create a slideshow with your uploaded media!" />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex min-h-screen flex-col items-center bg-[#A0AECD]">
         <header className="mb-8">
           <h1 className="text-black text-6xl mt-12">Shotlify</h1>
         </header>
+
         <main className="flex flex-col items-center">
           {videoSources ? videoSources.map((video, index) => {
-            return <video controls src={video} key={`video-${index}`} width={480} height={480} ></video>
+            return (
+              <div key={`video-${index}`}>
+                <video controls src={video} width={480} height={480}></video>
+                <button onClick={() => captureImageFromVideo(index)}>Get You a Screenshot</button>
+              </div>
+            )
           }) : ''}
 
           <FileInput accept={ACCEPTED_FILE_TYPES} onFilesChange={handleFileChange} hideFileChosen={true} />
 
           {imageSources ? imageSources.map((image, index) => {
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            return <Image src={image} alt={`Image Number ${index}. File Name: ${images[index]!.name}`} key={`img-${index}`} width={480} height={480}></Image>
+            return <Image src={image} alt={`Image Number ${index}.`} key={`img-${index}`} width={480} height={480}></Image>
           }) : ''}
         </main>
       </div>
