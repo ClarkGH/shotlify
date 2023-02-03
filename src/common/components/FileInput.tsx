f7a0af506d9f2f28d1bf88395e040aaa45254fe2
clarkgh <clarkhinchcliff@gmail.com>
2023-02-03 17:47:53 -0500

    minor clean-up

--- src/common/components/FileInput.tsx
+++ src/common/components/FileInput.tsx
@@ -15,7 +15,7 @@ const FileInput: FC<FileInputProps> = ({ onFilesChange, accept, hideFileChosen }
 
     return (
         <input
-            className={hideFileChosen ? 'text-transparent' : ''}
+            className="file:rounded file:border-0 file:text-md file:font-bold file:text-white file:bg-blue-500 hover:file:bg-blue-700 my-4"
             type="file"
             accept={accept}
             multiple 
